e94bb931b408f59f6f5fdfb622d58abc1cf5ac3d
sowjanyaseelam <sowjanyas132@gmail.com>
2022-02-03 19:42:57 +0530

    added my first file to repo

---
+++ addition.java
@@ -0,0 +1,9 @@
+hi sir
+iam sowjanya
+i have completed m.tech in power systems.
+i have a small family.
+i have samll baby girl,she is 2year old
+her name was aadhya prasastha.
+i have two elader brothers.
+my hobbies are reading books.
+my husband working as hospital field.
